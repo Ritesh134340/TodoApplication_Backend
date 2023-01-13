d3f6677054807a50d325febb41a297f0acd8726f
Ritesh134340 <riteshkumar134340@gmail.com>
2023-01-13 20:41:01 +0530

    google btn added

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const connection=require("./config/db");
-
 require("dotenv").config();
+require("./config/googleStrategy")
+
+
+const connection=require("./config/db");
 const express=require("express");
 const app=express();
 const cors=require("cors");
+const passport=require("passport")
+const session = require("express-session");
 
+app.use(cors({
+  origin:`${process.env.REACT_APP_URL}`,
+  methods:"GET,PUT,POST,DELETE,PATCH",
+  credentials:true
+}))
+
+
+app.use(
+  session({
+      secret:process.env.SECRET_KEY,
+      resave: false,
+      saveUninitialized: false,
+      maxAge:24*60*60*100
+  })
+);
+
+app.use(passport.initialize())
+app.use(passport.session());
+// app.use(cookieSession({name:"session",keys:['cyberwolve'],maxAge:24*60*60*100}));
+// app.use(passport.session());
 app.use("/images",express.static("images"))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -12,10 +36,12 @@ app.use(express.urlencoded({extended: true}));
 const PORT=process.env.PORT || 8080
 const userRouter=require("./routes/user.route")
 const todoRouter=require("./routes/todo.route");
+const googleRouter=require("./routes/google.route")
 
-app.use(cors())
+
 app.use("/user",userRouter)
 app.use("/todo",todoRouter)
+app.use("/auth",googleRouter)
 
 app.listen(PORT,async()=>{
     try{

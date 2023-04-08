a57f3df2f4e8792df0af931bd399452c3c60286e
Ritesh134340 <riteshkumar134340@gmail.com>
2023-04-08 08:47:52 +0530

    multer modified

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,37 +1,10 @@
 
 const multer = require("multer");
-const fs = require('fs');
 const path = require("path");
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "./images");
-//   },
-//   filename: (req, file, cb) => {
-//     cb(null, Date.now() + path.extname(file.originalname));
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports=upload
-
-
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    const destination = "./images";
-    fs.access(destination, (err) => {
-      if (err) {
-        // if directory does not exist, create it with permission '755'
-        fs.mkdir(destination, { recursive: true, mode: 0o755 }, (err) => {
-          if (err) throw err;
-          cb(null, destination);
-        });
-      } else {
-        cb(null, destination);
-      }
-    });
+    cb(null, "./images");
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
@@ -40,4 +13,8 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
-module.exports = upload;
+module.exports=upload
+
+
+
+

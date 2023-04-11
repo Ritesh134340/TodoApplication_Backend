fb6d8cf48775f0a9047544dd984862fbc83c22df
Ritesh134340 <riteshkumar134340@gmail.com>
2023-04-11 14:49:29 +0530

    client fixed

--- config/googleStrategy.js
+++ config/googleStrategy.js
@@ -6,7 +6,7 @@ const generator = require('generate-password');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:process.env.CLIENT_URL,
+    callbackURL:"https://todo-application-z9c7.onrender.com/auth/google/callback",
   },
   async function(accessToken, refreshToken, profile, done) {
     
